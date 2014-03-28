4068a1ac8a54b9fd47f97132783dac6c311bc1c5
aafzal1 <asnafzal@gmail.com>
2014-03-27 22:13:31 -0500

    Simple Server to perform query from a client

---
+++ socket_daemon/QueryDaemon.java
@@ -0,0 +1,55 @@
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+ 
+public class QueryDaemon {
+    public static void main(String[] args) throws IOException {
+         
+        if (args.length != 1) {
+            System.err.println("Usage: java QueryDaemon <port number>");
+            System.exit(1);
+        }
+ 
+        int portNumber = Integer.parseInt(args[0]);
+        boolean close_client = false;
+ 
+        try{ 
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+       // ) {
+         
+            String inputLine;
+            List<String> outputLine;
+             
+            // Initiate conversation with client
+            QueryProtocol qp = new QueryProtocol();
+            outputLine = qp.processInput(null);
+            for(String outL : outputLine)
+               out.println(outL);
+ 
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = qp.processInput(inputLine);
+                for(String outL : outputLine)
+                {
+                    out.println(outL);
+                    if(out.equals("Bye"))
+                      close_client = true;
+                }
+                //out.println(outputLine);
+                if (close_client == true)
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
