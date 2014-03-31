1602b891a61aa38ae03b15d155d0a1a278b74e28
ShruthiSudhanva <shruthi.mysore.s@gmail.com>
2014-03-31 00:46:18 -0500

    Intermediate changes to Average

--- mapreduce/Average.java
+++ mapreduce/Average.java
@@ -1,7 +1,9 @@
+package mapreduce;
+
 import java.io.IOException;
 import java.util.*;
 
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
@@ -16,13 +18,11 @@ public static class Map extends MapReduceBase implements Mapper<LongWritable, Te
     private IntWritable outputKey = new IntWritable();
 
     public int column;
-    /*Configure column parameter*/
+
     public void configure(JobConf job) {
          column = job.getInt("column",0);
          outputKey.set(column);
     }
-
-    /*Mapper passes output with only one key i.e. to only one reducer*/
     public void map(LongWritable key, Text value, OutputCollector<IntWritable, FloatWritable> output, Reporter reporter) throws IOException {
         String line = value.toString();
         String[] fields = line.split(",");
@@ -34,8 +34,6 @@ public static class Map extends MapReduceBase implements Mapper<LongWritable, Te
 
  public static class Reduce extends MapReduceBase implements Reducer<IntWritable, FloatWritable, IntWritable, FloatWritable> {
 
-
-    /*Loop through all values to get sum and count*/
     public void reduce(IntWritable key, Iterator<FloatWritable> values, OutputCollector<IntWritable, FloatWritable> output, Reporter reporter) throws
 IOException {
         float value, count = 0f;
@@ -48,27 +46,20 @@ IOException {
         float average = sum/count;
         output.collect(key, new FloatWritable(average));
     }
+
  }
-        
- public static void main(String[] args) throws Exception {
 
 
-	String input, output;
-        JobConf conf = new JobConf(Average.class);
-        conf.setJobName("average");
+    public void run(String inputPath, String outputPath, String key_index, String value_index) throws Exception
+    {
         /* Set column based on command line argument*/
-	if(args.length > 2)
-	{
-		int column = Integer.parseInt(args[0]);
-		conf.setInt("column",column);
-		input = args[1];
-		output = args[2];
-	}
-	else
-	{
-		input = args[0];
-		output = args[1];
-	}
+        JobConf conf = new JobConf(Average.class);
+        conf.set("mapred.job.tracker", "localhost:54311");
+        conf.set("fs.default.name", "hdfs://localhost:54310");
+        conf.setJobName("average");
+        int column = Integer.parseInt(value_index);
+        conf.setInt("column",column);
+
         conf.setOutputKeyClass(IntWritable.class);
         conf.setOutputValueClass(FloatWritable.class);
 
@@ -79,11 +70,27 @@ IOException {
         conf.set("mapred.textoutputformat.separator",",");
         conf.setOutputFormat(TextOutputFormat.class);
 
-        FileInputFormat.setInputPaths(conf, new Path(input));
-        FileOutputFormat.setOutputPath(conf, new Path(output));
-        //Average.column = 2;
-        conf.setFloat("mapred.snapshot.frequency", Float.parseFloat("0.50"));
+        //FileInputFormat.setInputPaths(conf, new Path(inputPath));
+        //FileOutputFormat.setOutputPath(conf, new Path(outputPath));
+
+        conf.setFloat("mapred.snapshot.frequency", Float.parseFloat("0.10"));
         conf.setBoolean("mapred.map.pipeline", true);
+        //Average.column = 2;
+
+        FileInputFormat.addInputPath(conf, new Path(inputPath));
+        Path out = new Path(outputPath);
+        FileSystem fs = FileSystem.get(conf);
+        fs.delete(out, true);
+        FileOutputFormat.setOutputPath(conf, out);
+
+
         JobClient.runJob(conf);
+    }
+        
+ public static void main(String[] args) throws Exception {
+
+    Average job = new Average();
+    job.run(args[0], args[1], args[2], args[3]);
+	
  }
 }
