4cc871626174b041b176cf302694fc09bf65722a
Vinit Shah <elysiums.shade@gmail.com>
2014-04-16 20:39:58 -0500

    Use the config to set and send server params

--- webserver/server.js
+++ webserver/server.js
@@ -1,18 +1,26 @@
 var express = require('express')
     , http = require('http')
     , path = require('path')
-    , client = require('./client-listener');
+    , client = require('./client-listener')
+    , config = require('./config');
 
 var app = express();
 
 // Configuration stuff
-app.set('port', '8080');
+app.set('port', config.server.port);
 app.use(express.static(path.join(__dirname, "ui/public")));
+
+// Endpoint for grabbing information about this server (like its url... which, in retrospect the client should know.. but.. #localhost_problems
+app.get('/serverinfo', function(req, res) {
+    res.json({
+        ip: config.server.ip,
+        port: config.server.port
+    });
+});
 
 // Send index.html on '/' requests
 app.get("/", function(req, res) {
-    res.send("hello world");
-    //res.sendfile(path.join(__dirname, "ui/public/index.html"));
+    res.sendfile(path.join(__dirname, "ui/public/index.html"));
 });
 
 var server = http.createServer(app);
