f921a55e7ac626b613edd943dc097d3cc0efd2ed
ShruthiSudhanva <shruthi.mysore.s@gmail.com>
2014-04-24 17:50:29 -0500

    Attributes in index.html

--- webserver/ui/public/js/site/guiSetup.js
+++ webserver/ui/public/js/site/guiSetup.js
@@ -4,9 +4,13 @@
 function guiSetup(socket) {
     var attributeCallback = function(attributes) {
         console.log("these are the attributes:", attributes);
-
+        var list = document.getElementById('attributes');
         for(var i = 0; i < attributes.length; i++) {
-            // Do something here with the attributes
+            var entry = document.createElement('a');
+            entry.href="#";
+            entry.className = "list-group-item";
+            entry.appendChild(document.createTextNode(attributes[i]));
+            list.appendChild(entry);
         }
     };
 
