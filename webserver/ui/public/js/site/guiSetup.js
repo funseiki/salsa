9e1359166c4a380d630189e063bb4cf7d77c60a7
Vinit Shah <elysiums.shade@gmail.com>
2014-04-23 21:03:16 -0500

    Sample code to get Shruthi started

---
+++ webserver/ui/public/js/site/guiSetup.js
@@ -0,0 +1,17 @@
+// Helper method for getting attributes
+function getAttributes(callback) {
+    // we're getting attributes....
+    // TODO: Use socket.io or a GET request to asynchronously grab attributes
+    var attributes = ['one', 'two', 'three'];
+    callback(attributes);
+}
+
+var attributeCallback = function(attributes) {
+    console.log("these are the attributes:", attributes);
+
+    for(var i = 0; i < attributes.length; i++) {
+        // Do something here with the attributes
+    }
+};
+
+getAttributes(attributeCallback);
