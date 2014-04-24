07b08e88e85f39c9223a159ee3d8c09f20b0893b
ShruthiSudhanva <shruthi.mysore.s@gmail.com>
2014-04-23 21:36:13 -0500

    Changes to guiSetup.js

--- webserver/ui/public/js/site/guiSetup.js
+++ webserver/ui/public/js/site/guiSetup.js
@@ -1,21 +1,26 @@
 // Helper method for getting attributes
-socket.on("attributes",function(data) {
-    
-});
+//Wrap everything in a function - guiSetup
 
-function getAttributes(callback) {
-    // we're getting attributes....
-    // TODO: Use socket.io or a GET request to asynchronously grab attributes
-    var attributes = ['one', 'two', 'three'];
-    callback(attributes);
-}
+function guiInput(socket) {
+    socket.on("attributes",function(data) {
 
-var attributeCallback = function(attributes) {
-    console.log("these are the attributes:", attributes);
+    });
 
-    for(var i = 0; i < attributes.length; i++) {
-        // Do something here with the attributes
+    function getAttributes(callback) {
+        // we're getting attributes....
+        // TODO: Use socket.io or a GET request to asynchronously grab attributes
+        var attributes = ['one', 'two', 'three'];
+        callback(attributes);
     }
-};
 
-getAttributes(attributeCallback);
+    var attributeCallback = function(attributes) {
+        console.log("these are the attributes:", attributes);
+
+        for(var i = 0; i < attributes.length; i++) {
+            // Do something here with the attributes
+        }
+    };
+
+    getAttributes(attributeCallback);
+}
+    
