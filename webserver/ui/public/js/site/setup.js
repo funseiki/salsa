0d0ceb2992f2823cc791a0417977243eae29f698
Vinit Shah <elysiums.shade@gmail.com>
2014-04-23 21:01:23 -0500

    Move javascript to a new file. Start on simple visualizations

---
+++ webserver/ui/public/js/site/setup.js
@@ -0,0 +1,25 @@
+function setupSocket(url) {
+    var socket = io.connect(url);
+    socket.on('news', function(data) {
+        console.log(data);
+        socket.emit('pokemon', {pokemon: 'bulbasaur'});
+    });
+}
+
+function setupViz() {
+    var data = [1, 2, 3, 4, 10, 12, 40, 3, 5, 1];
+    var viz = d3.select(".visualization");
+    var bar = viz.selectAll("div")
+                .data(data);
+    var barEnter = bar.enter().append("div")
+                    .style("width", function(d) { return d * 10 + "px"; })
+                    .text(function(d) { return d; });
+}
+
+$(document).ready(function() {
+    $.get('/serverinfo', function(data, status, xhr) {
+        var url = data.ip + ":" + data.port;
+        setupSocket(url);
+        setupViz();
+    });
+});
