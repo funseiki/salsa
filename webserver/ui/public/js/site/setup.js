b59e75ca198c8a2512a095e360b7ab8bca81dde2
Vinit Shah <elysiums.shade@gmail.com>
2014-04-23 21:37:50 -0500

    stuff

--- webserver/ui/public/js/site/setup.js
+++ webserver/ui/public/js/site/setup.js
@@ -1,4 +1,4 @@
-function setupSocket(url) {
+function setupSocket(url, callbacks) {
     var socket = io.connect(url);
     socket.on('news', function(data) {
         console.log(data);
@@ -8,6 +8,8 @@ function setupSocket(url) {
 
 function setupViz() {
     var data = [1, 2, 3, 4, 10, 12, 40, 3, 5, 1];
+    var width = 420,
+        barHeight = 20;
     var viz = d3.select(".visualization");
     var bar = viz.selectAll("div")
                 .data(data);
@@ -19,7 +21,7 @@ function setupViz() {
 $(document).ready(function() {
     $.get('/serverinfo', function(data, status, xhr) {
         var url = data.ip + ":" + data.port;
-        setupSocket(url);
+        setupSocket(url, []);
         setupViz();
     });
 });
