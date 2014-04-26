fa31ed2d63c945b08a442ffc5e3c5ff0689204bf
Vinit Shah <elysiums.shade@gmail.com>
2014-04-26 14:30:58 -0500

    Daemon listener should provide a small API Client listener should create a singleton Daemon Listener

--- webserver/listeners/client.js
+++ webserver/listeners/client.js
@@ -2,14 +2,42 @@
  * The portion of the webserver that will listen for socket.io request
  *  Messages from the client will be relayed to the daemon listener
  ****************************************************************************/
-var socketio = require("socket.io");
+var socketio = require("socket.io"),
+    daemon = new require("./daemon")();
 
-exports.listen = function(server) {
+exports.listen = function(server, daemonPort) {
     var io = socketio.listen(server);
-    io.sockets.on('connection', function(socket) {
-        socket.emit('news', {hello: 'world'});
-        socket.on('pokemon', function(data) {
+
+    if(daemonPort) {
+        daemon.listen(daemonPort);
+
+        // The daemon API
+        daemon.on('data', function(data) {
+            console.log("Response received: " + data.toString());
+            daemon.end();
+        });
+
+        daemon.on('end', function() {
+            console.log('daemon disconnected');
+        });
+
+        daemon.on('error', function(err) {
+            console.error(err);
+        });
+
+        daemon.on("data", function(data) {
             console.log(data);
+        });
+    }
+
+    io.sockets.on('connection', function(socket) {
+        // The client API
+        socket.emit('news', {hello: 'world'});
+
+        socket.on('query', function(query) {
+            console.log(data);
+            // TODO: First make sure we're not already sending a query
+            daemon.write(query);
         });
         socket.emit('attributes', ['attr1', 'attr2', 'att3']);
     });
--- webserver/listeners/daemon.js
+++ webserver/listeners/daemon.js
@@ -2,24 +2,43 @@
  * The portion of the webserver that will listen in on a local port.
  *  It will relay messages retrieved by the web server to the daemon process
  ****************************************************************************/
-var net = require('net');
+var net = require('net'),
+    EventEmitter = require("events").EventEmitter,
+    util = require('util');
 
-exports.listen = function(port){
-    var client = net.connect({port: port}, function() {
-        console.log('Client connected');
-        //client.write('sum 2 2');
-    });
+function DaemonListener() {
+    EventEmitter.call(this);
+}
 
-    client.on('data', function(data) {
-        console.log("Response received: " + data.toString());
-        client.end();
-    });
+util.inherits(DaemonListener, EventEmitter);
 
-    client.on('end', function() {
-        console.log('client disconnected');
-    });
+// Thin wrapper around 'net' object
+DaemonListener.prototype = {
+    listening: false,
+    client: null,
+    listen: function(port) {
+        var that = this;
+        this.client = net.connect({port: port}, function() {
+            console.log('Client connected');
+            that.listening = true;
+        });
 
-    client.on('error', function(err) {
-        console.error(err);
-    });
+        // Todo: Wrap all this events to higher level methods
+        this.client.on('data', function(data) {
+            that.emit('data', data);
+        });
+        this.client.on('end', function(data) {
+            that.emit('end', data);
+        });
+        this.client.on('error', function(data) {
+            that.emit('error', data);
+        });
+    },
+    write: function(dataString){
+        if(client) {
+            client.write(dataString);
+        }
+    }
 };
+
+module.exports = DaemonListener;
