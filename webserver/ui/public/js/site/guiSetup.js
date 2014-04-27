6133130a49b6313cf4bc87e925a88340f4d9814d
Vinit Shah <elysiums.shade@gmail.com>
2014-04-26 21:49:59 -0500

    Parse query (remove blank lines)

--- webserver/ui/public/js/site/guiSetup.js
+++ webserver/ui/public/js/site/guiSetup.js
@@ -1,14 +1,17 @@
 // Singleton for building portions of the UI
 var guiBuilder = {
-    buildAttributeList: function(attributes) {
+    buildAttributeList: function(data) {
+        var attributes = data.split("\n");
         console.log("these are the attributes:", attributes);
         var list = document.getElementById('attributes');
         for(var i = 0; i < attributes.length; i++) {
-            var entry = document.createElement('a');
-            entry.href="#";
-            entry.className = "list-group-item";
-            entry.appendChild(document.createTextNode(attributes[i]));
-            list.appendChild(entry);
+            if(attributes[i].length > 0) {
+                var entry = document.createElement('a');
+                entry.href="#";
+                entry.className = "list-group-item";
+                entry.appendChild(document.createTextNode(attributes[i]));
+                list.appendChild(entry);
+            }
         }
     },
 
