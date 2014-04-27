4a3835f890c3a3814c2e9409e6b0642237bfc57d
ShruthiSudhanva <shruthi.mysore.s@gmail.com>
2014-04-27 14:54:12 -0500

    Initial chart commit

--- webserver/ui/public/js/site/visualizer.js
+++ webserver/ui/public/js/site/visualizer.js
@@ -1,25 +1,74 @@
+
+data =  [[1,40,5,6],[4,50,7,8]];
+
+var chart;
+var w = 20,
+    h = 80;
+           
+var x = d3.scale.linear()
+    .domain([0, 1])
+    .range([0, w]);
+           
+var y = d3.scale.linear()
+    .domain([0, 100])
+    .rangeRound([0, h]);
+
 // Singleton which will handle visualizing things
 var visualizer = {
     clear: function() {
         // TODO: clear the visualization
     },
     startViz: function(shape, params) {
-        // TODO: add support for more visualizations
-        var data = [1, 2, 3, 4, 10, 12, 40, 3, 5, 1];
-        var width = 420,
-            barHeight = 20;
-        var viz = d3.select(".visualization");
-        var bar = viz.selectAll("div")
-                    .data(data);
-        var barEnter = bar.enter().append("div")
-                        .style("width", function(d) { return d * 10 + "px"; })
-                        .text(function(d) { return d; });
+                
+        chart = d3.select(".vis").append("svg")
+              .attr("class", "chart")
+              .attr("width", w*30 )
+              .attr("height", h);
+
+        chart.selectAll("rect")
+              .data(data)
+            .enter().append("rect")
+              .attr("x", function(d, i) { return x(i) - .5; })
+              .attr("y", function(d) { return h - y(d[1]) - .5; })
+              .attr("width", w)
+              .attr("height", function(d) { return y(d[1]); });
+
+        chart.append("line")
+              .attr("x1", 0)
+              .attr("x2", w * data.length)
+              .attr("y1", h - .5)
+              .attr("y2", h - .5)
+              .style("stroke", "#000");
+
     },
     update: function(data) {
         // Call this when there is data to update the visualization with
+        console.log(data);
+        chart.selectAll("rect")
+        .data(data)
+      .transition()
+        .duration(1000)
+        .attr("y", function(d) { 
+            return h - y(d[1]) - .5; })
+        .attr("height", function(d) {
+            return y(d[1]); });
+
 
     },
     done: function() {
         // Call this when the visualization is complete
     }
 }
+
+
+visualizer.startViz();
+visualizer.update(data);
+
+/*setInterval(function() {
+   data.shift();
+   data.push(next());
+   visualizer.update(data);
+ }, 1500);*/
+
+
+
