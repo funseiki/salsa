5ee7cb8e59bd72dac72f7e1d46d165495035ed80
Vinit Shah <elysiums.shade@gmail.com>
2014-04-26 22:42:13 -0500

    jQuery-ify attribute creation

--- webserver/ui/public/js/site/guiSetup.js
+++ webserver/ui/public/js/site/guiSetup.js
@@ -2,13 +2,13 @@
 var guiBuilder = {
     buildAttributeList: function(attributes) {
         console.log("these are the attributes:", attributes);
-        var list = document.getElementById('attributes');
+        var list = $('#attributes');
         for(var i = 0; i < attributes.length; i++) {
-            var entry = document.createElement('a');
-            entry.href="#";
-            entry.className = "list-group-item";
-            entry.appendChild(document.createTextNode(attributes[i]));
-            list.appendChild(entry);
+            var entry = $('<a ></a>');
+            entry.addClass("button list-group-item")
+            entry.attr("href","#");
+            entry.append(attributes[i])
+            list.append(entry);
         }
     },
 
