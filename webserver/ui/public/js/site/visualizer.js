4ad03ac02c19d287fb174a450cedd2344c78ce08
Vinit Shah <elysiums.shade@gmail.com>
2014-05-02 22:04:19 -0500

    Boss new api for stuff

--- webserver/ui/public/js/site/visualizer.js
+++ webserver/ui/public/js/site/visualizer.js
@@ -1,83 +1,41 @@
-data1 =  ["0,0,0,0","0,0,0,0","0,0,0,0"];
+var margin = {top: 20, right: 30, bottom: 30, left: 40}
+    width = 500 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
 
-var chart;
-var w = 50,
-    h = 200;
-           
-var x = d3.scale.linear()
-    .domain([0, 1])
-    .range([0, w]);
-           
-var y = d3.scale.linear()
-    .domain([0, 100])
-    .rangeRound([0, h]);
+var vis = d3.select('.vis')
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
 
 // Singleton which will handle visualizing things
 var visualizer = {
     clear: function() {
         // TODO: clear the visualization
     },
-    startViz: function(shape, params) {
-        data = this.convert(data1);
-        chart = d3.select(".vis").append("svg")
-              .attr("class", "chart")
-              .attr("width", w*30 )
-              .attr("height", h);
-
-        chart.selectAll("rect")
-              .data(data)
-            .enter().append("rect")
-              .attr("x", function(d, i) { return x(i) - .5; })
-              .attr("y", function(d) { return h - y(d[1]) - .5; })
-              .attr("width", w)
-              .attr("height", function(d) { return y(d[1]); });
-
-        chart.append("line")
-              .attr("x1", 0)
-              .attr("x2", w * data.length)
-              .attr("y1", h - .5)
-              .attr("y2", h - .5)
-              .style("stroke", "#000");
-
-    },
-    convert: function(data){
-        var newData = new Array();
-            var i;
-            console.log(data);
-            for(i=0;i<data.length;i++)
-            {
-                var numbers = data[i].split(",");
-                for (var j = 0; j <numbers.length; j++) {
-                    numbers[j] = parseInt(numbers[j]);
-                };
-                newData.push(numbers);
-            }
-            console.log(newData);
-            return newData;
+    addGraph: function(shape, params) {
+        if(!this.graphs) {
+            this.graphs = [];
+        }
+        switch(shape) {
+            case 'Bar':
+                this.graphs.push(new BarViz(height, width, margin, this.graphs.length));
+                break;
+            default:
+                break;
+        }
     },
     update: function(data) {
-        // Call this when there is data to update the visualization with
-        data = this.convert(data);
-        chart.selectAll("rect")
-        .data(data)
-      .transition()
-        .duration(1000)
-        .attr("y", function(d) { 
-            return h - y(d[1]) - .5; })
-        .attr("height", function(d) {
-            return y(d[1]); });
-
-
+        for(var index in this.graphs) {
+            this.graphs[index].update(data);
+        }
     },
     done: function() {
         // Call this when the visualization is complete
+        for(var index in this.graphs) {
+            this.graphs[index].done();
+        }
+    },
+    // Hide other graphs and show this one
+    show: function(id) {
+        // Do something
     }
 }
-
-
-visualizer.startViz();
-visualizer.update(data1);
-
-
-
-
