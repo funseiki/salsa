54b514c2dea2f406ca178c0130b2aeb7fb970005
Vinit Shah <elysiums.shade@gmail.com>
2014-05-04 19:39:17 -0500

    Visualizer handles adding line graphs. Also updating the global data

--- webserver/ui/public/js/site/visualizer.js
+++ webserver/ui/public/js/site/visualizer.js
@@ -1,11 +1,12 @@
 var margin = {top: 20, right: 30, bottom: 30, left: 100}
     width = 700 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
-        
+
 
 // Singleton which will handle visualizing things
 var visualizer = {
     clear: function() {
+        Viz.globalClear();
         // TODO: clear the visualization
     },
     addGraph: function(shape, params) {
@@ -17,11 +18,20 @@ var visualizer = {
                 console.log(params);
                 this.graphs.push(new BarViz(height, width, margin, params));
                 break;
+            case 'Line':
+                this.graphs.push(new LineViz(height, width, margin, params.id, params.groupBy));
+                break;
             default:
                 break;
         }
     },
     update: function(data) {
+        if(!data) {
+            data = [];
+        }
+        if(this.graphs.length > 0) {
+            Viz.globalUpdate(data);
+        }
         for(var index in this.graphs) {
             this.graphs[index].update(data);
         }
